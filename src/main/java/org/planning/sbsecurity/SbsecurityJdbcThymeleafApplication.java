c444f795e84bc39028da26764b126a2ca6d53cec
papaijvans <papaijvans@gmail.com>
2018-05-26 10:02:49 +0530

    Initial commit

---
+++ src/main/java/org/planning/sbsecurity/SbsecurityJdbcThymeleafApplication.java
@@ -0,0 +1,12 @@
+package org.planning.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecurityJdbcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsecurityJdbcThymeleafApplication.class, args);
+	}
+}
